83c8085bcf94bbe3e926c46a8e413581ec5cac5b
efandor <firstdir@gmail.com>
2020-09-30 11:01:11 +0300

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+   
+  if (!array || array[0] === undefined) {
+      return 0;
+  } else {
+    let minItem = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minItem) {
+            minItem = array[i];
+        }
+    }
+    return minItem;
+  }
+}
+ 
+exports.max = function max(array) {
+  if (!array || array[0] === undefined) {
+      return 0;
+  }
+  let maxItem = array[0];
+  for (let i = 1; i < array.length; i++) {
+      if (array[i] > maxItem) {
+          maxItem = array[i];
+      }
+  }
+  return maxItem;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array[0] === undefined) {
+    return 0;
+  } else {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+    sum +=array[i]
+    }
+    return sum /array.length;
+  }
 }
